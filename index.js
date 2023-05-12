d497b13e7a781834855ce2d52c449472ce1f87d5
enricmartinvila <97675689+enricmartinvila@users.noreply.github.com>
2023-05-12 17:03:54 +0200

    commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-const router = require('./routes/route')
+const router = require('./routes/route');
 app.use(express.json());
 const cors = require('cors');
+//import .env
+require('dotenv').config();
 app.use(cors());
 app.use('/api/v1', router);
 
-app.listen(3000, function () {
-    console.log(`Example app listening on port 3000!`);
+app.listen(process.env.PORT , function () {
+    console.log(`Example app listening on port ` + process.env.PORT);
 });
